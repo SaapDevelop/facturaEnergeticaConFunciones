612dcdcfe81b0a2ebb20b2f889b6dffcc96e547a
SaapDevelop <saapdevelop@gmail.com>
2025-08-16 09:22:38 -0500

    Comenzamos con el archivo que desarrollará el ejercico de factura energetica con funciones

---
+++ script.js
@@ -0,0 +1,22 @@
+//Una empresa de energía necesita simular la generación de una factura mensual simple. Para ello, deberás construir un sistema básico que calcule el valor a pagar por un cliente utilizando exclusivamente funciones sin parámetros, todas con retorno, y declaradas de diferentes formas: declarativas, de expresión y funciones flecha.
+
+//REQUERIMIENTOS
+
+
+/* CONDISIONES
+
+-Todas las funciones deben usar return.
+
+-Ninguna función puede tener parámetros ni argumentos.
+
+-Ninguna función debe usar objetos, arreglos, clases o this.
+
+-Todas las funciones deben ser invocadas al menos una vez, y algunas pueden llamarse dentro de otras.
+
+Los valores deben estar definidos directamente dentro de las funciones.
+
+El enfoque del ejercicio es comparar la forma y el comportamiento de cada tipo de función.
+
+El proyecto debe estar en un repositorio de GitHub con una buena convención de nomenclatura(solo camelCase) y un commit por función.
+
+*/
